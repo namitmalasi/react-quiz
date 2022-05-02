381ef68efc555ed66956f9c39eff602addc919e7
Namit Malasi <namitmalasi@gmail.com>
2022-05-02 13:53:18 +0530

    footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
         <Header />
       </div>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      Made with ❤ by{" "}
+      <a
+        href="https://namit-malasi.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Namit Malasi
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
