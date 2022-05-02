e11984abac78b30493a1d54f5bf26022718c488a
Namit Malasi <namitmalasi@gmail.com>
2022-05-02 13:08:43 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}></div>
+    <BrowserRouter>
+      <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
